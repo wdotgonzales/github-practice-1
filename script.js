5073f74396952f203a3f256b15fdc320e0a58068
Wilson Gonzales <wdotgonzales@gmail.com>
2024-02-22 04:49:21 +0800

    add countdown feature

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const sayGreetings = () => {
-    alert('Hello World!!!!')
+    let countdown = 10;
+
+    let intervalId = setInterval(() => {
+        countdown--;
+        console.log(countdown + 1); // Log the countdown value
+
+        if (countdown === 0) {
+            console.log('Happy New Year!!! :)')
+            clearInterval(intervalId); // Stop the interval when countdown is 1
+        }
+    }, 1000);
 }
 
-sayGreetings();
+sayGreetings(); // Call the function to start the countdown
