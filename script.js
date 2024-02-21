083f65ba7e1e444e4f453cecadc7efa5c4713745
Wilson Gonzales <wdotgonzales@gmail.com>
2024-02-22 05:28:31 +0800

    add more console logs

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const sayGreetings = () => {
-    let countdown = 10;
+    let countdown = 15;
 
     let intervalId = setInterval(() => {
         countdown--;
         console.log(countdown + 1); // Log the countdown value
+        console.log("I'm still counting down");
 
         if (countdown === 0) {
+            console.logZ("I'm down counting! lol")
             console.log('Happy New Year!!! :)')
             clearInterval(intervalId); // Stop the interval when countdown is 1
         }
