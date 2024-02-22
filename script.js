00f0cd011629e4dace2baf9d90f4e0ab743162cb
Wilson Gonzales <wdotgonzales@gmail.com>
2024-02-22 08:44:15 +0800

    create different greeting (nword)

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const sayGreetings = () => {
-    alert('Hello World!!!!')
+    let nWord = 'nigga';
+    let nWordArr = nWord.split('');
+    let index = 0;
+    let intervalId;
+
+    intervalId = setInterval(() => {
+        if (index < nWord.length) {
+            console.log(nWordArr[index]);
+            index++;
+        } else {
+            clearInterval(intervalId);
+        }
+    }, 1000);
 }
 
 sayGreetings();
